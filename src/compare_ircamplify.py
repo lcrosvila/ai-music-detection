191a5cad972d3d3e9ff8e940035eb77a9338d46f
lcrosvila <34547568+lcrosvila@users.noreply.github.com>
2024-11-18 14:31:21 +0100

    Update compare_ircamplify.py

--- src/compare_ircamplify.py
+++ src/compare_ircamplify.py
@@ -3,39 +3,11 @@ import numpy as np
 import json
 import os
 import pickle
-from concurrent.futures import ThreadPoolExecutor, as_completed
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import classification_report
-
-def load_single_embedding(file):
-    if file.endswith('.npy'):
-        return np.load(file, mmap_mode='r')
-    return None
-
-def load_embeddings(files):
-    valid_files = [f for f in files if f.endswith('.npy')]
-    
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(load_single_embedding, file) for file in valid_files]
-        embeddings = [future.result() for future in as_completed(futures) if future.result() is not None]
-    
-    return np.array(embeddings)
-
-def get_split(split, embedding, folders):
-    files = []
-    y = []
-    for folder in folders:
-        with open(f'/data/{folder}/{split}.txt', 'r') as f:
-            folder_files = f.read().splitlines()
-        
-        files.extend([f'/data/{folder}/audio/embeddings/{embedding}/{file}.npy' for file in folder_files])
-        y.extend([folder] * len(folder_files))
-    
-    X = load_embeddings(files)
-    y = np.array(y)
-    return X, y, files
+from utils.data_utils import get_split
 
 def load_ircamplify_results(folders):
     true_class = []
